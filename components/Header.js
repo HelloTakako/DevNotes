fa28e33e722bf5b952c85d18d70294232652a4b4
Takako <24834291+HelloTakako@users.noreply.github.com>
2022-06-18 03:57:23 -0700

    removed styled-component library

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,8 @@
-import { useEffect } from 'react';
-import styled from 'styled-components';
-
 import Link from 'next/link';
-import MenuIcon from '@material-ui/icons/Menu';
 
 export default function Header() {
   return (
-    <Nav>
+    <nav>
       <ul>
         <li>
           <Link href="/">
@@ -49,39 +45,6 @@ export default function Header() {
           </Link>
         </li>
       </ul>
-    </Nav>
+    </nav>
   );
 }
-
-const Nav = styled.header`
-  width: 100%;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #222;
-  margin-bottom: 50px;
-  ul {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-    padding: 0;
-  }
-  li {
-    list-style: none;
-    padding: 0 15px;
-    border-right: 1px solid #888;
-  }
-  li:last-child {
-    border-right: none;
-  }
-  li a {
-    text-decoration: none;
-    color: #fff;
-    transition: 0.3s;
-  }
-  li a:hover {
-    opacity: 0.6;
-  }
-`;
