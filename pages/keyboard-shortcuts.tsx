2b77a137d4e40461d55ad2ceb0f38a854a69d15d
Takako <24834291+HelloTakako@users.noreply.github.com>
2023-12-26 02:40:33 -0800

    enphasized commands

--- pages/keyboard-shortcuts.tsx
+++ pages/keyboard-shortcuts.tsx
@@ -6,58 +6,159 @@ import Footer from '../components/Footer';
 export default function KeyboardShortcuts() {
   return (
     <div>
-      <main className='regular-width-container'>
+      <main className="regular-width-container">
         <h2>Useful VS Code keyboard Shortcuts</h2>
-        <ul>
-          <li>Area focus : Shift + Cursors</li>
-          <li>Move to the start/end of the line : Cmd + Cursors</li>
-          <li>Move to previous/next word : Option + left / right cursor</li>
-          <li>Copy : Cmd + C</li>
-          <li>Paste : Cmd + V</li>
-          <li>Cut : Cmd + X</li>
-          <li>Undo a step : Cmd + Z</li>
-          <li>Redo a step : Cmd + Shift + Z</li>
-          <li>Multiple select : Cmd + D</li>
-          <li>Search : Cmd + F </li>
-          <li>Project search : Cmd + Shift+ F</li>
-          <li>Refer the declaration origin : F12</li>
-          <li>Peaking: Refer the declaration origin : Option + F12</li>
-          <li>Display reference : Shift + F12</li>
-          <li>Quick fix : Cmd + .</li>
-          <li>Search files : Cmd + P</li>
-          <li>Close the current tab : Cmd + W</li>
-          <li>Save the current file : Cmd + S</li>
-          <li>Move the line up/down : Option + up/down cursor</li>
-          <li>Move to other files opened : Cmd + Option + left/right cursor</li>
-          <li>Toggle line comment : Cmd + /</li>
-          <li>Display command pallet : Cmd + Shift + P / F1</li>
-          <li>Open a new window : Cmd + Shift + N</li>
-          <li>Close the folder : Cmd + K -&gt; F</li>
-          <li>Open a new tab : Cmd + N</li>
-          <li>Save with another name : Cmd + Shift + S</li>
-          <li>Reopen a previously closed tab : Cmd + Shift + T</li>
-          <li>Open VSCode settings : Cmd + ,</li>
-          <li>Chanage symbol name : F2</li>
-          <li>Search symbol name over all files : Cmd + T</li>
-          <li>Move to the symbols in the file : Cmd + Shift + O</li>
-          <li>Move to the start/end of brackets : Cmd + Shift + \</li>
-          <li>Select all which matched in search : Cmd + F2</li>
-          <li>Toggle a block comment : Option + Shift + A</li>
-          <li>Replace strings in the file : Cmd + Option + F</li>
-          <li>See change history : Ctrl + Tab</li>
-          <li>Open the previously edited file : Ctrl + _ / Ctrl + -</li>
-          <li>Open a file : Cmd + O</li>
-          <li>Zoom in : Cmd + Shift + -</li>
-          <li>Zoom out : Cmd + -</li>
-          <li>Delete a line : Cmd + Shift + K</li>
-          <li>Copy a line to above/below : Option + Shift + up/down cursor</li>
-          <li>Move to specific line : Ctrl + G</li>
-          <li>Toggle side bar : Cmd + B</li>
-          <li>Move to next problem : F8</li>
-          <li>Move to previous problem : Shift + F8</li>
-          <li>Select current line : Cmd + L</li>
-          <li>Save all tabs : Cmd + Option + S</li>
-          <li>Copy the current file&apos;s path : Cmd + K -&gt; P</li>
+        <ul className="keyboard-shortcuts">
+          <li>
+            Area focus : <span>Shift + Cursors</span>
+          </li>
+          <li>
+            Move to the start/end of the line : <span>Cmd + Cursors</span>
+          </li>
+          <li>
+            Move to previous/next word :{' '}
+            <span>Option + left / right cursor</span>
+          </li>
+          <li>
+            Copy : <span>Cmd + C</span>
+          </li>
+          <li>
+            Paste : <span>Cmd + V</span>
+          </li>
+          <li>
+            Cut : <span>Cmd + X</span>
+          </li>
+          <li>
+            Undo a step : <span>Cmd + Z</span>
+          </li>
+          <li>
+            Redo a step : <span>Cmd + Shift + Z</span>
+          </li>
+          <li>
+            Multiple select : <span>Cmd + D</span>
+          </li>
+          <li>
+            Search : <span>Cmd + F </span>
+          </li>
+          <li>
+            Project search : <span>Cmd + Shift+ F</span>
+          </li>
+          <li>
+            Refer the declaration origin : <span>F12</span>
+          </li>
+          <li>
+            Peaking - Refer the declaration origin : <span>Option + F12</span>
+          </li>
+          <li>
+            Display reference : <span>Shift + F12</span>
+          </li>
+          <li>
+            Quick fix : <span>Cmd + .</span>
+          </li>
+          <li>
+            Search files : <span>Cmd + P</span>
+          </li>
+          <li>
+            Close the current tab : <span>Cmd + W</span>
+          </li>
+          <li>
+            Save the current file : <span>Cmd + S</span>
+          </li>
+          <li>
+            Move the line up/down : <span>Option + up/down cursor</span>
+          </li>
+          <li>
+            Move to other files opened :{' '}
+            <span>Cmd + Option + left/right cursor</span>
+          </li>
+          <li>
+            Toggle line comment : <span>Cmd + /</span>
+          </li>
+          <li>
+            Display command pallet : <span>Cmd + Shift + P / F1</span>
+          </li>
+          <li>
+            Open a new window : <span>Cmd + Shift + N</span>
+          </li>
+          <li>
+            Close the folder : <span>Cmd + K -&gt; F</span>
+          </li>
+          <li>
+            Open a new tab : <span>Cmd + N</span>
+          </li>
+          <li>
+            Save with another name : <span>Cmd + Shift + S</span>
+          </li>
+          <li>
+            Reopen a previously closed tab : <span>Cmd + Shift + T</span>
+          </li>
+          <li>
+            Open VSCode settings : <span>Cmd + ,</span>
+          </li>
+          <li>
+            Chanage symbol name : <span>F2</span>
+          </li>
+          <li>
+            Search symbol name over all files : <span>Cmd + T</span>
+          </li>
+          <li>
+            Move to the symbols in the file : <span>Cmd + Shift + O</span>
+          </li>
+          <li>
+            Move to the start/end of brackets : <span>Cmd + Shift + \</span>
+          </li>
+          <li>
+            Select all which matched in search : <span>Cmd + F2</span>
+          </li>
+          <li>
+            Toggle a block comment : <span>Option + Shift + A</span>
+          </li>
+          <li>
+            Replace strings in the file : <span>Cmd + Option + F</span>
+          </li>
+          <li>
+            See change history : <span>Ctrl + Tab</span>
+          </li>
+          <li>
+            Open the previously edited file : <span>Ctrl + _ / Ctrl + -</span>
+          </li>
+          <li>
+            Open a file : <span>Cmd + O</span>
+          </li>
+          <li>
+            Zoom in : <span>Cmd + Shift + -</span>
+          </li>
+          <li>
+            Zoom out : <span>Cmd + -</span>
+          </li>
+          <li>
+            Delete a line : <span>Cmd + Shift + K</span>
+          </li>
+          <li>
+            Copy a line to above/below :{' '}
+            <span>Option + Shift + up/down cursor</span>
+          </li>
+          <li>
+            Move to specific line : <span>Ctrl + G</span>
+          </li>
+          <li>
+            Toggle side bar : <span>Cmd + B</span>
+          </li>
+          <li>
+            Move to next problem : <span>F8</span>
+          </li>
+          <li>
+            Move to previous problem : <span>Shift + F8</span>
+          </li>
+          <li>
+            Select current line : <span>Cmd + L</span>
+          </li>
+          <li>
+            Save all tabs : <span>Cmd + Option + S</span>
+          </li>
+          <li>
+            Copy the current file&apos;s path : <span>Cmd + K -&gt; P</span>
+          </li>
         </ul>
         <br />
         <br />
@@ -65,6 +166,12 @@ export default function KeyboardShortcuts() {
       </main>
 
       <Footer />
+      <style jsx global>{`
+        .keyboard-shortcuts span {
+          font-size: 120%;
+          font-weight: bold;
+        }
+      `}</style>
     </div>
   );
 }
